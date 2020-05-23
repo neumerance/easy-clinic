1690d5585c664558458cbbbd7b8287a8d58e29dd
Jonathan Canaveral <jonathan.canaveral@moneysmart.com>
2020-05-23 11:51:00 +0800

    conversation controller, specs and serializers

---
+++ spec/serializers/conversation_serializer_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe ConversationSerializer do
+  let(:patient) { create(:patient) }
+  let(:patient_case) { create(:patient_case) }
+  let(:conversation) do create(
+    :conversation, :with_attachments,
+    message_for: patient_case, user: patient.user
+  )
+  end
+
+  subject do
+    described_class.new(conversation, {
+      params: {
+        include_assoc: true
+      }
+    }).serializable_hash.dig(:data, :attributes)
+  end
+
+  context 'serializables' do
+    it 'has attributes' do
+      expect(subject.dig(:content)).to eq conversation.content
+      expect(subject.dig(:is_read)).to eq conversation.is_read
+      expect(subject.dig(:owner, :name)).to eq patient.profile.name
+      expect(subject.dig(:owner, :photo)).not_to be_empty
+    end
+  end
+end
